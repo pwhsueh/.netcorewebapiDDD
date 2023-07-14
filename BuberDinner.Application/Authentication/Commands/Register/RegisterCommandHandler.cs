9ee8e881023d4b5f9a1286100d015ac344602c6f
PWHsueh <pwhsueh@gmail.com>
2023-07-14 15:42:57 +0800

    Flow Control | ASP.NET 6 REST API Following CLEAN ARCHITECTURE & DDD Tutorial | Part 6

---
+++ BuberDinner.Application/Authentication/Commands/Register/RegisterCommandHandler.cs
@@ -0,0 +1,48 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Persistence;
+using ErrorOr;
+using MediatR;
+using BuberDinner.Domain.Common.Errors;
+using BuberDinner.Domain.Entities;
+using BuberDinner.Application.Common;
+
+namespace BuberDinner.Application.Authentication.Commands.Register;
+
+public class RegisterCommandHandler : IRequestHandler<RegisterCommand, ErrorOr<AuthenticationResult>>
+{
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
+
+    public RegisterCommandHandler(
+        IJwtTokenGenerator jwtTokenGenerator,
+        IUserRepository userRepository)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+        _userRepository = userRepository;
+    }
+
+    public async Task<ErrorOr<AuthenticationResult>> Handle(RegisterCommand command, CancellationToken cancellationToken)
+    {
+        // 1. Validate the user doesn't exist
+        if (_userRepository.GetByEmail(command.Email) is not null)
+        {
+            return Errors.User.DuplicateEmail;
+        }
+
+        // 2. create user (generate unique id) & persist to DB
+        var user = new User
+        {
+            FirstName = command.FirstName,
+            LastName = command.LastName,
+            Email = command.Email,
+            Password = command.Password
+        };
+        _userRepository.Add(user);
+
+        // 3. create JTWT token
+
+        var token = _jwtTokenGenerator.GenerateToken(user);
+
+        return new AuthenticationResult(user, token);
+    }
+}
---
+++ BuberDinner.Application/Authentication/Queries/Login/LoginQueryHandler.cs
@@ -0,0 +1,43 @@
+using BuberDinner.Application.Common;
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Domain.Common.Errors;
+using BuberDinner.Domain.Entities;
+using ErrorOr;
+using MediatR;
+
+namespace BuberDinner.Application.Authentication.Queries.Login;
+
+public class LoginQueryHandler : IRequestHandler<LoginQuery, ErrorOr<AuthenticationResult>>
+{
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
+
+    public LoginQueryHandler(
+        IJwtTokenGenerator jwtTokenGenerator,
+        IUserRepository userRepository)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+        _userRepository = userRepository;
+    }
+
+    public async Task<ErrorOr<AuthenticationResult>> Handle(LoginQuery query, CancellationToken cancellationToken)
+    {
+        // 1. Validate the user exists
+        if (_userRepository.GetByEmail(query.Email) is not User user)
+        {
+            return Errors.Authentication.InvalidCredentials;
+        }
+
+        // 2. Vlaidate the password is correct
+        if (user.Password != query.Password)
+        {
+            return new[] { Errors.Authentication.InvalidCredentials };
+        }
+
+        var token = _jwtTokenGenerator.GenerateToken(user);
+
+        // 3. create JTWT token
+        return new AuthenticationResult(user, token);
+    }
+}
